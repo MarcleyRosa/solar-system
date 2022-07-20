45691b27236d2bfd06ea40e724aa2873693c3a10
Marcley <marcleyrosa@gmail.com>
2022-07-20 16:39:10 -0300

    Requisito 4

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import Title from './components/Title';
 
 class App extends React.Component {
   render() {
-    const textHead = 'test';
     return (
       <div>
         <Header />
         <SolarSystem />
-        <Title headline={ textHead } />
       </div>);
   }
 }
