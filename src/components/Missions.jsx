8b2290e669f3c105e44c0d14a4769a1287643944
Marcley <marcleyrosa@gmail.com>
2022-07-20 17:47:18 -0300

    Requisito 7 e 8

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -1,9 +1,14 @@
-// import React, { Component } from "react";
+import React, { Component } from 'react';
+import Title from './Title';
 
-// class Missions extends Component {
-//   render() {
-//     return ()
-//   }
-// }
+class Missions extends Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headline="Missões" />
+      </div>
+    );
+  }
+}
 
-// export default Missions;
+export default Missions;
